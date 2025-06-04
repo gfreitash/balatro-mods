cde5b00242f726d718cdeec955cb82a02a002f31
Gabriel Freitas <gfreitash@gmail.com>
2025-06-04 19:52:15 -0300

    Migrate rebalanced-stakes to take_ownership and update documentation
    - Replace Lovely patches with take_ownership for stake modifications
    - Add CHANGELOG.md following Keep a Changelog format
    - Restructure README with clearer sections (Key Features, Installation, Design Notes)
    - Remove outdated Lovely patch references from documentation

--- rebalanced-stakes/main.lua
+++ rebalanced-stakes/main.lua
@@ -1,1 +1,21 @@
--- Empty for now
+SMODS.Blind:take_ownership('blue', {
+    modifiers = function ()
+        G.GAME.modifiers.enable_perishables_in_shop = true
+    end
+})
+
+-- Orange stake (7)
+SMODS.Blind:take_ownership('orange', {
+    modifiers = function()
+        G.GAME.modifiers.enable_rentals_in_shop = true
+    end
+})
+
+-- Gold stake (8)
+SMODS.Blind:take_ownership('gold', {
+    modifiers = function()
+        G.GAME.win_ante = 9
+        G.GAME.interest_base = 6
+        G.GAME.interest_cap = 30
+    end
+})
